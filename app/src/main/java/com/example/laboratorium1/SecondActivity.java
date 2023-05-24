7a4c10a3efe75021ee6e808810cee7d2b6c265bc
vyhivskyi <rushoskey@gmail.com>
2023-05-24 17:52:21 +0200

    k

--- app/src/main/java/com/example/laboratorium1/SecondActivity.java
+++ app/src/main/java/com/example/laboratorium1/SecondActivity.java
@@ -6,23 +6,28 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
+import com.example.laboratorium1.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class SecondActivity extends AppCompatActivity {
 
-    private TextView mTextViewOcena;
     Button button;
+    ArrayList<ModelOceny> mListaOcen;
+    String[] nazwyPrzedmiotow;
+    int liczbaOcen;
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -34,6 +39,7 @@ public class SecondActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    RecyclerView recyclerView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,14 +47,16 @@ public class SecondActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Bundle pakunek = intent.getExtras();
-        int liczbaOcen = pakunek.getInt("liczbaOcen");
-        String[] nazwyPrzedmiotow = getResources().getStringArray(R.array.przedmioty);
-
-        InteraktywnyAdapterTablicy adapter = new InteraktywnyAdapterTablicy(this, liczbaOcen, nazwyPrzedmiotow);
-        RecyclerView recyclerView = findViewById(R.id.lista_ocen_rv);
+        liczbaOcen = pakunek.getInt("liczbaOcen");
+        nazwyPrzedmiotow = getResources().getStringArray(R.array.przedmioty);
+        mListaOcen = new ArrayList<>();
+        for (int i = 0; i < liczbaOcen; i++) {
+            mListaOcen.add(new ModelOceny(nazwyPrzedmiotow[i], 2));
+        }
+        InteraktywnyAdapterTablicy adapter = new InteraktywnyAdapterTablicy(this, mListaOcen);
+        recyclerView = findViewById(R.id.lista_ocen_rv);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         button = findViewById(R.id.przyciskSrednia);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -60,6 +68,31 @@ public class SecondActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        if(savedInstanceState != null) {
+            for (int i=0; i<savedInstanceState.getInt("liczbaOcen"); i++) {
+                mListaOcen.add(new ModelOceny(nazwyPrzedmiotow[i], savedInstanceState.getInt("ocena"+i)));
+            }
+            recyclerView.setAdapter(new InteraktywnyAdapterTablicy(this, mListaOcen));
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("liczbaOcen", liczbaOcen);
+        for (int i=0; i<liczbaOcen; i++) {
+            outState.putInt("ocena"+i, mListaOcen.get(i).getOcena());
+        }
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        for (int i=0; i<savedInstanceState.getInt("liczbaOcen"); i++) {
+            mListaOcen.add(new ModelOceny(nazwyPrzedmiotow[i], savedInstanceState.getInt("ocena"+i)));
+        }
+        recyclerView.setAdapter(new InteraktywnyAdapterTablicy(this, mListaOcen));
     }
 
 }
